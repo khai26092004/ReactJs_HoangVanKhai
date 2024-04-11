eb9f72a4e7065b7e60eb9f66e7f6be33ecf19fca
khai26092004 <150600288+khai26092004@users.noreply.github.com>
2024-04-11 17:16:39 +0700

    lesson3

--- lesson3/hvk-demo/src/App.js
+++ lesson3/hvk-demo/src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const users = {
+    userName:"k22cnt4",
+    password:"123456@",
+    fullname:"hoangvankhai"
+  }
   return (
     <div className="App">
       
